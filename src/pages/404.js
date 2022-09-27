52e072636bf389300143f4394ee7772eb388b80a
오지훈 <hihiwwg@naver.com>
2022-09-27 14:16:37 +0900

    Initialize env/scss

---
+++ src/pages/404.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { graphql } from "gatsby";
+
+import Layout from "../components/layout/index";
+import SEO from "../components/seo";
+
+import "../styles/404.scss";
+
+const NotFoundPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title;
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="404: Not Found" />
+      <h1 className="h1">404: Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
